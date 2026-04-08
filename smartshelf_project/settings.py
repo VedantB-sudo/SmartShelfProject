681d2720c0d37f2e56804c305a79f56addc6f382
EC2 Default User <ec2-user@ip-172-31-51-206.ec2.internal>
2026-04-08 19:15:30 +0000

    oaihoaihgqepighoeq

--- smartshelf_project/settings.py
+++ smartshelf_project/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory',
-    'storages',  # Required for AWS S3
+    'storages',  # Required for AWS S4
 ]
 
 MIDDLEWARE = [
