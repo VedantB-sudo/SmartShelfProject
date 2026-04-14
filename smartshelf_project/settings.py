89cd60ce4e6c2b3421ecdc3d74e103f557a34ffe
EC2 Default User <ec2-user@ip-172-31-51-206.ec2.internal>
2026-04-14 11:33:48 +0000

    oigweobigfw

--- smartshelf_project/settings.py
+++ smartshelf_project/settings.py
@@ -87,6 +87,8 @@ AWS_DEFAULT_ACL = None
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
+CSRF_TRUSTED_ORIGINS = ['http://smartcloud.us-east-1.elasticbeanstalk.com/']
+
 # Using S3 for Media files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
